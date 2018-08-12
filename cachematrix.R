eb3700eefe85d2ba5c64097f131e61dfd936c0cb
verdetamadachi <31346513+verdetamadachi@users.noreply.github.com>
2018-08-12 09:20:37 +0100

    Finally works! :)
    Finally made it work as it should.

--- cachematrix.R
+++ cachematrix.R
@@ -4,7 +4,29 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        
+        #InvercedMatrix: matrix object to store inversed matrix 
+        InversedMatrix <- NULL
+        
+        
+        setCache <- function(y){
+                x <<- y
+                InversedMatrix <<- NULL
+        } 
+        
+        getCache <- function() {x}
+        
+        #===== Inversed matrix set and get functions
+        
+        setInversedMatrix <- function(matrix) {
+                InvercedMatrix <<- matrix
+        }
+        
+        getInversedMatrix <- function() {
+                InversedMatrix}
+        
+        
+        list(setCache = setCache, getCache = getCache, setInversedMatrix = setInversedMatrix, getInversedMatrix = getInversedMatrix )
 }
 
 
@@ -12,4 +34,17 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        
+        m <- x$getInversedMatrix()
+        if(!is.null(m)) {
+                message("getting cached data")
+                return(m)
+        }
+        else{
+                data <- x$getCache()
+                m <- solve(data, ...)
+                x$setInversedMatrix(m)
+                m
+        }
+        
 }
